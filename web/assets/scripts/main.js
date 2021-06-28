9b7178d24be60255c08778f6165db318b61cad23
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-28 10:55:07 -0300

    add main.js

---
+++ web/assets/scripts/main.js
@@ -0,0 +1,7 @@
+import { watchNavLinks } from './watchLinks.js';
+import { watchMenu } from './watchMenu.js';
+import { watchScroll } from './watchScroll.js';
+
+watchMenu();
+watchNavLinks();
+watchScroll();
