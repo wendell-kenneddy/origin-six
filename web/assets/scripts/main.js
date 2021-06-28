fe4b1105fb7caa19a44ea34233c189869f1cfbd9
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-28 17:18:58 -0300

    import swiper and scrollreveal modules

--- web/assets/scripts/main.js
+++ web/assets/scripts/main.js
@@ -1,6 +1,8 @@
 import { watchNavLinks } from './watchLinks.js';
 import { watchMenu } from './watchMenu.js';
 import { watchScroll } from './watchScroll.js';
+import { swiper } from './swiper.js';
+import { scrollReveal } from './scrollReveal.js';
 
 watchMenu();
 watchNavLinks();
