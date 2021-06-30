aa5f6706beeb3cc72ad08c2986716a416610d4e1
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-30 08:12:37 -0300

    add watchActiveSection function to handleScroll

--- web/assets/scripts/handleScroll.js
+++ web/assets/scripts/handleScroll.js
@@ -1,10 +1,12 @@
 import { handleBackToTopBtn } from './handleBackToTopBtn.js';
 import { changeHeaderWhenScroll } from './changeHeaderWhenScroll.js';
+import { watchActiveSection } from './watchActiveSection.js';
 
 const handleScroll = () => {
   window.addEventListener('scroll', e => {
     changeHeaderWhenScroll();
     handleBackToTopBtn();
+    watchActiveSection();
   });
 };
 
