ac803a8b4b10f5cf97167d68197412d60149cab5
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-30 08:11:46 -0300

    add watchActiveSection function

---
+++ web/assets/scripts/watchActiveSection.js
@@ -0,0 +1,26 @@
+const sections = document.querySelectorAll('main section[id]');
+
+const watchActiveSection = () => {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active');
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active');
+    }
+  }
+};
+
+export { watchActiveSection };
