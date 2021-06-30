af8ad13ddde2951f11b9544caa4a569db64f9687
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-30 08:13:40 -0300

    update changeHeaderWhenScroll

--- web/assets/scripts/changeHeaderWhenScroll.js
+++ web/assets/scripts/changeHeaderWhenScroll.js
@@ -1,7 +1,7 @@
-const changeHeaderWhenScroll = () => {
-  const pageHeader = document.querySelector('header#page-header');
-  const scrollHeight = pageHeader.offsetHeight;
+const pageHeader = document.querySelector('header#page-header');
+const scrollHeight = pageHeader.offsetHeight;
 
+const changeHeaderWhenScroll = () => {
   return window.scrollY > scrollHeight
     ? pageHeader.classList.add('scroll')
     : pageHeader.classList.remove('scroll');
